9e04804e0f726ad7beceb9bcd73baa01c0e9af9e
Carlos Porras <aporras80366@ufide.ac.cr>
2026-07-06 17:26:58 -0600

    Nuevos archivos.
    Mostrar mensaje al tocar el boton de comprar
    Añadir  .gitignore para que en los commits y cambios no se suban archivos o folders innecesarios

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function comprar(cafe){
-
-alert("Has seleccionado: " + cafe);
-
+function comprar(cafe) {
+    fetch("/comprar", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ cafe: cafe })
+    })
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("mensaje").innerText = data.mensaje;
+    })
+    .catch(() => {
+        document.getElementById("mensaje").innerText = "No se pudo conectar con el servidor.";
+    });
 }
